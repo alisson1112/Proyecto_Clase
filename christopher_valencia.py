21ba0146f44800df466d5a8f1ed359b957dd4e0f
OscarAlegria12 <inusukesan60@gmail.com>
2026-06-29 14:41:48 -0600

    se creo la lista

---
+++ christopher_valencia.py
@@ -0,0 +1,9 @@
+mi_lista=[]
+Objetos=["Palo", "Computadora", "Cortina", "Cuaderno"]
+
+for i in range(0,1,3):
+    print(Objetos[0])
+    print(Objetos[-1])
+    print(Objetos[-2])
+    print(Objetos[-3])
+ 
